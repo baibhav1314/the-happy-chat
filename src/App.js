df682b7431a13eb8f74da90e6c442fec31aaa676
baibhav1314 <baibhav.p1314@gmail.com>
2022-03-10 16:31:37 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    const [{ user }, dispatch] = useStateValue();
+    return (
+        <div className="app">
+            {!user ? (
+                // <Login />
+                <h1>hi</h1>
+            ) : (
+                <div className="app_body">
+                    <Router>
+                        <Sidebar />
+                        <Switch>
+                            <Route path="/rooms/:roomId">
+                                <Chat />
+                            </Route>
+                            <Route path="/">
+                                <Chat />
+                            </Route>
+                        </Switch>
+                    </Router>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
